74ac1c863ab65563c3969350da2d46fcf022c964
Shajid <shajidsr12@gmail.com>
2026-03-29 20:06:53 +0600

    solve valid parenthesis

---
+++ stack/13-valid-parenthesis.js
@@ -0,0 +1,31 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isValid(s) {
+        let stack = []
+        const bracketMap = {
+            '}' : '{' ,
+            ')' : '(' ,
+            ']' : '[' ,
+        }
+
+        for (let i = 0 ; i < s.length ;i++){
+            let char = s[i]
+            if(bracketMap[char]){
+                let topElement = stack.length > 0 ? stack.pop() : '#'
+                if(topElement !== bracketMap[char]){
+                    return false
+                }
+              
+            }
+            else{
+                stack.push(char)
+            }
+
+        }
+        return stack.length === 0
+    } 
+    
+}
