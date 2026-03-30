b35678aa5bf9e5bcd384a37558e23af2e58f3edd
Shajid <shajidsr12@gmail.com>
2026-03-30 14:25:37 +0600

    add carFleet solution

---
+++ stack/17-car-fleet.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number} target
+ * @param {number[]} position
+ * @param {number[]} speed
+ * @return {number}
+ */
+var carFleet = function(target, position, speed) {
+    let stack = []
+        let cars = []
+        for(let i = 0 ; i < position.length ; i++){
+            cars.push([position[i],speed[i]])
+        }
+        cars.sort((a,b) => b[0]-a[0])
+
+
+        for(let i = 0 ; i < cars.length ; i++ ){
+            let carPosition = cars[i][0]
+            let carSpeed = cars[i][1]
+
+            let arrivalTime = (target - carPosition ) / carSpeed 
+
+            if (stack.length === 0 || arrivalTime > stack[stack.length - 1]) {
+                stack.push(arrivalTime);
+            }
+        }
+        return stack.length
+
+};
